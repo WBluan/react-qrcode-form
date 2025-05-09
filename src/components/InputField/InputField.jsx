88b36f540cdeccc038f90b8cb8c0776da97051ed
luan.weber <lu062372@intelbras.com.br>
2025-05-09 08:29:04 -0300

    feat: add custom checkbox to password inputs

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,17 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './InputField.module.scss';
 import { FormContext } from '../../context/FormContext';
 
 const InputField = ({ label, name, type = 'text'}) => {
     const { formData, updateField} = useContext(FormContext);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const isPassword = type === 'password';
     return (
         <div className={styles.inputGroup}>
             <label>{label}</label>
             <input 
-                type={type}
+                type={isPassword && showPassword ? 'text' : type}
                 value={formData[name]}
                 onChange={e => updateField(name, e.target.value)}
             />
+
+        {isPassword && (
+            <label className={styles.checkBox}>
+                <input 
+                    type="checkbox"
+                    checked={showPassword}
+                    onChange={() => setShowPassword(prev => !prev)}
+                />
+                Exibir Senha
+            </label>
+        )}
         </div>
     );
 };
